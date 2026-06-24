de185925be90dd6d79dfffff369badaa6793f4fe
Andrewjs14 <100407635+Andrewjs14@users.noreply.github.com>
2026-06-24 21:18:22 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import { ThemeProvider } from '@/components/theme-provider'
 const archivo = Archivo({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'John Doe',
+  title: 'Andrew Jaya Susilo | Data Analyst',
 }
 
 export default function RootLayout({
